21d116c308ee65fa0881bd81e751ad7f691c11c9
gullariskandarova <iskenderovaguller777@gmail.com>
2024-01-23 17:37:45 +0400

    final

--- TimeZone/assets/js/admin.js
+++ TimeZone/assets/js/admin.js
@@ -2,6 +2,9 @@ const BASE_URL = "http://localhost:8080";
 const form = document.querySelector("form");
 const tbody = document.querySelector("tbody");
 const allInputs = document.querySelectorAll("input");
+const search = document.querySelector(".search");
+const sort = document.querySelector(".sort");
+let productCopy;
 async function getAllData() {
   try {
     let res = await axios(`${BASE_URL}/products`);
@@ -13,6 +16,7 @@ async function getAllData() {
 }
 getAllData();
 function drawTable(data) {
+  tbody.innerHTML = "";
   data.forEach((element) => {
     let tr = document.createElement("tr");
     tr.innerHTML += `
@@ -20,20 +24,56 @@ function drawTable(data) {
     <td><img src="${element.imageURL}" alt=""></td>
     <td>${element.title}</td>
     <td>$${element.price}</td>
-    <td><i class="fa-solid fa-trash"></i></td>
+    <td><i class="fa-solid fa-trash" onclick=deleteProduct("${element.id}",this)></i></td>
     `;
     tbody.append(tr);
   });
 }
 
-form.addEventListener("submit", async function () {
-  if (allInputs[3] > 0) {
+form.addEventListener("submit", async function (e) {
+  e.preventDefault();
+  if (allInputs[3].value > 0) {
     let newProduct = {
       imageURL: allInputs[0].value,
       title: allInputs[1].value,
       desc: allInputs[2].value,
       price: allInputs[3].value,
     };
-    axios.post(`${BASE_URL}/products`, newProduct);
+    await axios.post(`${BASE_URL}/products`, newProduct);
+    allInputs[0].value = "";
+    allInputs[0].value = "";
+    allInputs[0].value = "";
+    allInputs[0].value = "";
+  } else {
+    alert("Price is Negative");
   }
+});
+
+async function deleteProduct(id, btn) {
+  if (confirm("ARE U SURE")) {
+    await axios.delete(`${BASE_URL}/products/${id}`);
+    btn.closest(".card");
+  }
+}
+
+search.addEventListener("input", function (e) {
+  let filtered = productCopy.filter((item) =>
+    item.title.toLocaleLowerCase().includes(e.target.value.toLocaleLowerCase())
+  );
+  return drawTable(filtered);
+});
+
+sort.addEventListener("click", function () {
+  let sorted;
+  if (this.innerText === "ASC") {
+    this.innerText = "DESC";
+    sorted = productCopy.sort((a, b) => a.title.localeCompare(b.title));
+  } else if (this.innerText === "DESC") {
+    this.innerText = "DEFAULT";
+    sorted = productCopy.sort((a, b) => b.title.localeCompare(a.title));
+  } else {
+    this.innerText = "ASC";
+    sorted = productCopy;
+  }
+  drawTable(sorted);
 });
---
+++ TimeZone/assets/js/detail.js
@@ -0,0 +1,24 @@
+const id = new URLSearchParams(window.location.search).get("id");
+const BASE_URL = "http://localhost:8080";
+const detail = document.querySelector(".detail");
+async function getData() {
+  try {
+    let res = await axios(`${BASE_URL}/products/${id}`);
+    drawDetail(res.data);
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+getData();
+function drawDetail(data) {
+  detail.innerHTML = `
+    <div class="card">
+    <img src="${data.imageURL}" alt="">
+    <div class="content">
+        <h1 class="title">${data.title}</h1>
+        <p class="desc">${data.desc}</p>
+        <p class="price">${data.price}</p>
+    </div>
+</div>
+    `;
+}
